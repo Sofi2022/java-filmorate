648fd61eddb9b094c89f02c749cfebdedeccc19a
Sofi2022 <sofa.kirikova@yandex.ru>
2022-09-20 17:32:26 +0300

    Все замечания исправлены, добавлены аннотации, убран метод increaseId(), написаны тесты только для метода валидации

--- src/test/java/ru/yandex/practicum/filmorate/controllers/FilmControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controllers/FilmControllerTest.java
@@ -2,9 +2,6 @@ package ru.yandex.practicum.filmorate.controllers;
 
 import org.junit.jupiter.api.Test;
 
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-
 import ru.yandex.practicum.filmorate.Exceptions.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 
@@ -12,8 +9,6 @@ import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
-@AutoConfigureMockMvc
 public class FilmControllerTest {
 	@Test
 	void validateFilmTest() throws ValidationException {
